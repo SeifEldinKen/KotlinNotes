88fbdc8e6fc031dc8ab793ccca64da957d567e74
Seif Eldin <seifeldinrefaat@gmail.com>
2021-08-23 02:07:57 +0200

    init project

---
+++ app/src/main/java/com/neouto/kotlinnotes/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.neouto.kotlinnotes.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.neouto.kotlinnotes.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
