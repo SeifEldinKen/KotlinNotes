b690bb9cb79ea7869672a09c16ae0ec0f5e34f6a
Seif Eldin <seifeldinrefaat@gmail.com>
2021-08-23 20:01:14 +0200

    create note viewModel

---
+++ app/src/main/java/com/neouto/kotlinnotes/viewmodel/NoteViewModel.kt
@@ -0,0 +1,35 @@
+package com.neouto.kotlinnotes.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.neouto.kotlinnotes.data.model.Note
+import com.neouto.kotlinnotes.data.repository.NoteRepository
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.Dispatchers
+
+class NoteViewModel(private val repository: NoteRepository): ViewModel() {
+
+    // --> all note live data
+    val fetchAllNoteLiveData: LiveData<List<Note>> by lazy {
+        repository.fetchAllNotesLiveData
+    }
+    
+    // --> insert note to database
+    fun insertNote(note: Note) {
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.insertNote(note)
+        }
+    }
+
+    // --> delete note form database
+    fun deleteNote(note: Note) {
+        repository.deleteNote(note)
+    }
+
+    // --> delete all database
+    fun deleteAllNotes() {
+        repository.deleteAllNotes()
+    }
+
+}
