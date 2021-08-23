1c414d5c65bc173115c6760ac995a6113ad0ddb0
Seif Eldin <seifeldinrefaat@gmail.com>
2021-08-23 20:21:35 +0200

    ADD navigation graph

--- app/src/main/java/com/neouto/kotlinnotes/ui/MainActivity.kt
+++ app/src/main/java/com/neouto/kotlinnotes/ui/MainActivity.kt
@@ -2,11 +2,35 @@ package com.neouto.kotlinnotes.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 import com.neouto.kotlinnotes.R
+import com.neouto.kotlinnotes.data.db.NoteDatabase
+import com.neouto.kotlinnotes.data.repository.NoteRepository
+import com.neouto.kotlinnotes.databinding.ActivityMainBinding
+import com.neouto.kotlinnotes.viewmodel.NoteViewModel
+import com.neouto.kotlinnotes.viewmodel.NoteViewModelFactory
 
 class MainActivity : AppCompatActivity() {
+
+    // --> Init view binding
+    private val binding: ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
+    // --> Init repository
+    private val repository: NoteRepository by lazy {
+        NoteRepository(NoteDatabase.getInstance(this).noteDao())
+    }
+
+    // --> Init viewModel
+    private val noteViewModel: NoteViewModel by lazy {
+        ViewModelProvider(this, NoteViewModelFactory(repository))
+            .get(NoteViewModel::class.java)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+
+
     }
 }
